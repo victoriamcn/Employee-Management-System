248c3eccde8fe554f7a900567bfd2649f29eaea9
Victoria <vmcnorrill@gmail.com>
2023-04-15 21:35:00 -0400

    still buggy ponyboy

--- utils/viewEmployees.js
+++ utils/viewEmployees.js
@@ -1,11 +1,20 @@
 const connection = require('../db/connections');
 
-connection.query("SELECT first_name, last_name FROM employee", function (err, result, fields) { 
-    // INNER JOIN roles USING (title)
-    //your function here what you want to do with that data/result
-    if (err) {
-        console.log('There was an error getting your results.')
-        console.info(err)
-    }
-    console.log(result)
-});
+// connection.query("SELECT first_name, last_name FROM employee", function (error, result, fields) { 
+//     // INNER JOIN roles USING (title)
+//     //your function here what you want to do with that data/result
+//     if (error) {
+//         console.log('There was an error getting your results.')
+//         console.info(error)
+//     }
+//     //console.log(result)
+// });
+
+function viewEmployees(){
+    const [rows] =  connection.query('SELECT * FROM employee')
+    return rows
+}
+const employees =  viewEmployees()
+console.log(employees)
+
+module.exports = viewEmployees
